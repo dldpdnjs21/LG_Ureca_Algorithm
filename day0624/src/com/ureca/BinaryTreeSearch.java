5e48ff0dee7620d8028557453224601e6b1bb1f5
student <student@M4029>
2024-06-24 16:38:31 +0900

    분할정복

--- day0624/src/com/ureca/BinaryTreeSearch.java
+++ day0624/src/com/ureca/BinaryTreeSearch.java
@@ -91,3 +91,22 @@ public class BinaryTreeSearch<T> { //<> : 제네릭표현방법 => 자료형을 
 	}	
 	
 }
+
+
+//int[] coin = new int[N];
+//
+//for(int i = 0; i < N; i++) {
+//	coin[i] = in.nextInt();
+//}
+//
+//int count = 0;
+//for(int i = N - 1; i >= 0; i--) {
+//	// 현재 동전의 가치가 K보다 작거나 같아야지 구성가능하다.
+//	if(coin[i] <= K) {
+//		// 현재 가치의 동전으로 구성할 수 있는 개수를 더해준다.
+//		count += (K / coin[i]);
+//		K = K % coin[i];
+//	}
+//}
+//System.out.println(count);
+//}
