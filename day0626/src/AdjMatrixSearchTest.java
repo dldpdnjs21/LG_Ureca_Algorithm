467c0607d7f58fd6f372ce984e97237685dfc490
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-06-26 14:39:50 +0900

    인접행렬_BFS탐색

--- day0626/src/AdjMatrixSearchTest.java
+++ day0626/src/AdjMatrixSearchTest.java
@@ -24,8 +24,13 @@ public class AdjMatrixSearchTest {
 		System.out.println("======bfs1(너비우선탐색)======");
 		bfs1();
 		
+		System.out.println("======bfs2(너비우선탐색2)======");
+		bfs2();
+		
 		sc.close();
 	}//main
+	
+	//큐에서 나갈때 방문처리
 	private static void bfs1() {
 		//1.큐생성
 //		Queue<Integer> queue = new LinkedList<>();
@@ -39,15 +44,57 @@ public class AdjMatrixSearchTest {
 		//3.대기열의 정점 방문(탐색) ==> 큐가 빈상태일때까지 탐색
 		while(!queue.isEmpty()) {//한개 이상의 정점정보가 존재한다면
 			current = queue.poll();//가장 먼저 입력된 정점을 얻어오기
+			
+			//>>>>>>>>>>>>중복추가발생 방지
+			if(visited[current]) {
+				System.out.println("이미 처리된 정점입니다");
+				continue;
+			}
+			
+			//4.방문처리
 			visited[current] = true;//방문(처리)된 것을 체크
 			System.out.println((char)(current+'A'));//형변환
 			
+			//5.자식정점(인접정점)찾기
 			for(int i=0; i<N; i++) {//인접한 노드들 ==> 큐에 입력
 				if(!visited[i] && adjMatrix[current][i]) {//현재 노드에 인접하였다면
 					queue.offer(i);
-				}//if
-			}//for
+				}
+			}
 		}//while
 		
 	}//bfs1
+	
+	//큐에 들어갈때 방문처리
+	private static void bfs2() {
+		//1.큐생성
+//		Queue<Integer> queue = new LinkedList<>();
+		Queue<Integer> queue = new ArrayDeque<>();
+		boolean visited[] = new boolean[N];
+		
+		//2.시작정점 추가
+		int current = 0;//시작정점(인덱스)을 표현
+		queue.offer(current);
+
+		//3.방문처리
+		visited[current] = true;//방문(처리)된 것을 체크
+		
+		//4.대기열의 정점 방문(탐색) ==> 큐가 빈상태일때까지 탐색
+		while(!queue.isEmpty()) {//한개 이상의 정점정보가 존재한다면
+			current = queue.poll();//가장 먼저 입력된 정점을 얻어오기
+			
+			
+			System.out.println((char)(current+'A'));//형변환
+			
+			
+			//5.자식정점(인접정점)찾기
+			for(int i=0; i<N; i++) {//인접한 노드들 ==> 큐에 입력
+				if(!visited[i] && adjMatrix[current][i]) {//현재 노드에 인접하였다면
+					queue.offer(i);
+					visited[i] = true;//방문(처리)된 것을 체크
+				}
+			}
+		}//while
+		
+	}//bfs2
 }//end class
